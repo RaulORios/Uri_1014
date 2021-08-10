174c92c16857708cd3b69fcce84f1054de9e72e4
RaulORios <raul.orios89@outlook.com>
2021-08-10 11:41:06 -0300

    Arquivos atualizados

---
+++ Uri_1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            decimal y = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            decimal consumo = x / y;
+
+            Console.WriteLine($"{consumo.ToString("F3",CultureInfo.InvariantCulture)} km/l");           
+
+        }
+    }
+}
